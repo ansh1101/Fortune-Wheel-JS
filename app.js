acf72a9a3f896602f0c9952f65de482be15f7ce8
Ansh Singh <81492907+ansh1101@users.noreply.github.com>
2021-09-23 21:58:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-(function() {
+
+  
+  (function() {
     var wheel = document.querySelector('.wheel');
     let marker = document.querySelector('.marker');
     var startButton = document.querySelector('.button');
@@ -8,20 +10,28 @@
 
     let partionAngle ={
 
-        1: "Congratulations You Won Apple Music Gift Card",
-        2: " Congratulations You Won Xbox Gift Card",
-        3: "Congratulations You Won Steam Gift Card",
-        4: "Congratulations You Won Google Gift Card",
-        5: "Congratulations You Won Amazon Gift Card",
-        6: "Congratulations You Won Nintendo Gift Card",
-        7: "Congratulations You Won WIndows Gift Card",
-        8: "Congratulations You Won Glaistics Gift Card"
+        1: " You Won Apple Music Gift Card",
+        2: "  You Won Xbox Gift Card",
+        3: " You Won Steam Gift Card",
+        4: " You Won Google Gift Card",
+        5: " You Won Amazon Gift Card",
+        6: " You Won Nintendo Gift Card",
+        7: " You Won WIndows Gift Card",
+        8: " You Won Glaistics Gift Card"
         
     }
 
     let spinPrizes = (actualDeg) =>{
         let winner = Math.ceil(actualDeg / prizesPartionAngle); 
-        alert(partionAngle[winner]);
+        Swal.fire({
+            title: "Congratulations ",
+            text: partionAngle[winner],
+           
+            confirmButtonText: 'Click to spin more',
+            confirmButtonText : window.setTimeout(function my(){location.reload()},5000)
+          })
+
+       
     }
 
     startButton.addEventListener('click', () => {
@@ -34,13 +44,62 @@
     
   
     wheel.addEventListener('transitionend', () => {
+  
+
       startButton.style.pointerEvents = 'auto';
       wheel.style.transition = 'none';
       var actualDeg = deg % 360;
+ 
+      document.querySelector(".myDiv").style.visibility = "hidden";
       wheel.style.transform = `rotate(${actualDeg}deg)`;
       spinPrizes(actualDeg);
     });
   })();
   
+// Init code
+  // (function() {
+//     var wheel = document.querySelector('.wheel');
+//     let marker = document.querySelector('.marker');
+//     var startButton = document.querySelector('.button');
+//     let display = document.querySelector(".display");
+//     let deg = 0;
+//     let prizesPartionAngle = 45;
+
+//     let partionAngle ={
+
+//         1: "Congratulations You Won Apple Music Gift Card",
+//         2: " Congratulations You Won Xbox Gift Card",
+//         3: "Congratulations You Won Steam Gift Card",
+//         4: "Congratulations You Won Google Gift Card",
+//         5: "Congratulations You Won Amazon Gift Card",
+//         6: "Congratulations You Won Nintendo Gift Card",
+//         7: "Congratulations You Won WIndows Gift Card",
+//         8: "Congratulations You Won Glaistics Gift Card"
+        
+//     }
+
+//     let spinPrizes = (actualDeg) =>{
+//         let winner = Math.ceil(actualDeg / prizesPartionAngle); 
+//         alert(partionAngle[winner]);
+//     }
+
+//     startButton.addEventListener('click', () => {
+//       startButton.style.pointerEvents = 'none';
+//       deg = Math.floor(3000 + Math.random() * 5000);
+
+//       wheel.style.transition = 'all 10s ease-out';
+//       wheel.style.transform = `rotate(${deg}deg)`;
+//     });
+    
   
+//     wheel.addEventListener('transitionend', () => {
+//       startButton.style.pointerEvents = 'auto';
+//       wheel.style.transition = 'none';
+//       var actualDeg = deg % 360;
+//       wheel.style.transform = `rotate(${actualDeg}deg)`;
+//       spinPrizes(actualDeg);
+//     });
+//   })();
+ 
+
  
